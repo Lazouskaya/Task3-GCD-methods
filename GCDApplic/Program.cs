da1e548347ef611de5f4871ca4c0d93df79736fb
Viktoryia Lazouskaya <Viktoryia_Lazouskaya@epam.com>
2015-07-27 17:41:42 +0300

    add task3

---
+++ GCDApplic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using GCDMethods;
+
+
+namespace GCDApplic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan time = new TimeSpan();
+            Console.WriteLine(Nod.Evclidean(12, 16, out time));
+            Console.WriteLine(time);
+            Console.WriteLine(Nod.Evclidean(out time, 18, 6, 27, 36));
+            Console.WriteLine(time);
+            Console.WriteLine(Nod.Stain(16, 12, out time));
+            Console.WriteLine(time);
+            Console.WriteLine(Nod.Stain(out time, 18, 6, 27, 36, 0, 0));
+            Console.WriteLine(time);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestGCDMethods/TestGCD.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using GCDMethods;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+
+namespace TestGCDMethods
+{
+    [TestFixture]
+    public class EvklidAlgorithmTest
+    {
+        private TimeSpan time=new TimeSpan();
+        [TestCase(49, 28, Result = 7)]
+        [TestCase(0, 0, Result = 0)]
+        [TestCase(0, 1, Result = 1)]
+        [Test]
+        public int GCDByEvclideanFor2numbers(int number1, int number2)
+        {
+            return Nod.Evclidean(number1, number2, out time);
+        }
+
+        [TestCase(25, 115, 65, 80, Result = 5)]
+        [Test]
+        public int GCDByEvclideanForamount( params int[] numbers)
+        {
+            return Nod.Evclidean(out time, numbers);
+        }
+
+        [TestCase(48, 30, Result = 6)]
+        [TestCase(0, 0, Result = 0)]
+        [TestCase(0, 1, Result = 1)]
+        [Test]
+        public int GCDByStainFor2numbers(int number1, int number2)
+        {
+            return Nod.Stain(number1, number2, out time);
+        }
+
+        [TestCase(78, 294, 570, 36, Result = 6)]
+        [Test]
+        public int FindGCDByStaineAlgorithmTest(params int[] numbers)
+        {
+            return Nod.Stain(out time,numbers);
+        }
+    }
+}
